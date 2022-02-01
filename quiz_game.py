d5fd11a8af52bedba7981572e3de58ade60b558b
Natnael <natnaelebuy@gmail.com>
2022-01-31 20:52:54 -0800

    update guiz game

--- quiz_game.py
+++ quiz_game.py
@@ -1,4 +1,27 @@
 print("Welcome to my computer!")
 
 playing = input(" Do you want to play? ")
-print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play:)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print('Correct!')
+else:
+    print("Incorrect!")
+
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print('Correct!')
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print('Correct!')
+else:
+    print("Incorrect!")
